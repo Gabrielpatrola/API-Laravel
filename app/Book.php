95169783c6661c34dc5ff8bfdbd751db9e4e4842
gabrielpatrola <almeidagabrielaugusto@gmail.com>
2019-07-16 09:35:56 -0400

    models feitos

--- app/Book.php
+++ app/Book.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Rating;
 
 class Book extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function ratings()
+    {
+        return $this->hasMany(Rating::class);
+    }
 }
--- app/Rating.php
+++ app/Rating.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Book;
 
 class Rating extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
