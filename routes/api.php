085a046de77859712a28f517d95b98a80f0c0bf3
gabrielpatrola <almeidagabrielaugusto@gmail.com>
2019-07-17 09:04:18 -0400

    final

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +10,15 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::namespace('API')->name('api.')->group(function(){
+	Route::prefix('products')->group(function(){
+		Route::get('/', 'ProductController@index')->name('index_products');
+		Route::get('/{id}', 'ProductController@show')->name('single_products');
+		Route::post('/', 'ProductController@store')->name('store_products');
+		Route::put('/{id}', 'ProductController@update')->name('update_products');
+		Route::delete('/{id}', 'ProductController@delete')->name('delete_products');
+	});
 });
